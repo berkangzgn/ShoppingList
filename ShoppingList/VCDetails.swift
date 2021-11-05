8bb2097ce1ec46aa7c25627508a223334280431b
berkangzgn <bgezgin@protonmail.com>
2021-11-05 13:39:11 +0300

    Notification center added

--- ShoppingList/VCDetails.swift
+++ ShoppingList/VCDetails.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
+import CoreData
 
 class VCDetails: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
     
     @IBOutlet weak var productView: UIImageView!
     @IBOutlet weak var productName: UITextField!
@@ -30,10 +30,12 @@ class VCDetails: UIViewController, UIImagePickerControllerDelegate, UINavigation
         productView.addGestureRecognizer(imageGestureRecognizer)
     }
     
+    
     // Klavyeyi kapatma fonksiyonu
     @objc func closeKeyboard() {
         view.endEditing(true)
     }
+    
     
     // Resim seçme fonk
     @objc func chooseImage() {
@@ -47,14 +49,46 @@ class VCDetails: UIViewController, UIImagePickerControllerDelegate, UINavigation
         present(picker, animated: true, completion: nil)
     }
 
+    
+    // Resmi seçtikten sonra ekleme ekranına seçilen resmi ekleme
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         productView.image = info[.originalImage] as? UIImage
         // pickerController kapatmak için
         self.dismiss(animated: true, completion: nil)
     }
     
+    
     @IBAction func saveButton(_ sender: Any) {
+        // Kayıt işlemlerimizi AppDelegate altındaki context metoduyla yapıyoruz. Alttaki kodlar yardımızyla oraya erişiyoruz.
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let shopping = NSEntityDescription.insertNewObject(forEntityName: "ShoppingList", into: context)
         
+        
+        // DB ürün özlk ekleme
+        shopping.setValue(productName.text!, forKey: "name")
+        shopping.setValue(brand.text!, forKey: "brand")
+        if let price = Int(price.text!) {
+            shopping.setValue(price, forKey: "price")
+        }
+        shopping.setValue(UUID(), forKey: "id")
+            // compressionQuality : Sıkıştırma kalitesi
+        let data = productView.image!.jpegData(compressionQuality: 0.5)
+        shopping.setValue(data, forKey: "image")
+        
+        // Kayıt kontrol
+        do {
+            try context.save()
+            print("kayıt okeyto beybito")
+        } catch{
+            print("hata var goççum")
+        }
+        
+        // Girilen bilgileri post etme
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "dataAdded"), object : nil)
+        // Ürün ekledikten sonra ana ekrana geri dönme
+        self.navigationController?.popViewController(animated: true)
     }
+    
+    
     
 }
--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -6,22 +6,86 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
     
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(saveButtonClicked))
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Sağ üstteki ekleme butonu
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        giveData()
     }
     
-    @objc func saveButtonClicked(){
+    
+    // Seelctor - dataAdded bildiirmi gelirse ne yapacağı
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(giveData), name: NSNotification.Name(rawValue: "dataAdded"), object: nil)
+    }
+    
+    
+    @objc func giveData() {
+        // Aynı üürnden 2 ve daha fazla eklenirse ana ekranda bunların filtrelenmesi
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        // DB bağlanma
+        let context = (UIApplication.shared.delegate as? AppDelegate)!.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ShoppingList")
+        // Çok büyük verileri çekerken cashing mekanizmasından faydalanmaya yarar
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            // Any döndüğü için - try context.fetch - dönüştürdük.
+            let sonuclar = try context.fetch(fetchRequest)
+            
+            // sonuclar'ı ANy'den NSManagedObjectContext dönüşümünü yapıtoruz.
+            for sonuc in sonuclar as! [NSManagedObject]{
+                    // Verdiğimiz anahtar kelime bize istediğimiz sonucu verecektir.
+                if let name = sonuc.value(forKey: "name") as? String {
+                    nameArray.append(name)
+                }
+                if let id = sonuc.value(forKey: "id") as? UUID {
+                    idArray.append(id)
+                }
+            }
+            
+            // Verileri güncelledikten sonra tabloyu güncellemesi için
+            tableView.reloadData()
+            
+        } catch {
+            print("hata var goççum")
+        }
+        
+    }
+    
+    
+    @objc func addButtonClicked(){
      performSegue(withIdentifier: "toVCDetails", sender: nil)
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
 }
-
